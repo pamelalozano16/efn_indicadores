2e89b76651ad9b9fa1d2357b75113cfdbdf2e7d9
pamelalozano16 <30474787+pamelalozano16@users.noreply.github.com>
2019-11-13 15:36:19 -0600

    Error iterador

--- public/index.js
+++ public/index.js
@@ -1,19 +1,19 @@
 
 async function getData(){
 try{
-  //  const dataJSON = await fetch('http://localhost:5000/api/indicadores')
+   //const dataJSON = await fetch('http://localhost:5000/api/indicadores')
 
-    const dataJSON = await fetch('http://indicadores.efactornetwork.com:8080/api/indicadores')
+   const dataJSON = await fetch('http://indicadores.efactornetwork.com:8080/api/indicadores')
     const data = await dataJSON.json()
 console.log(data)
 for(var i in data){
-    // id = data[i].id
+    //id = data[i].id
     id = data[i].Id
-
     var arr=$("."+id);
-    for(var i=0;i<arr.length;i++){
-        // arr[i].innerHTML=data[i].valor;
-        arr[i].innerHTML=data[i].Valor;
+    console.log(arr)
+    for(var j=0;j<arr.length;j++){
+         //arr[j].innerHTML=data[i].valor;
+       arr[j].innerHTML=data[i].Valor;
     }
 }
 
